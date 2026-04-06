5d62f5ea425df15b33642be500e2d3fd70091aa7
Jairo Martinez Tello <150602520+AdminSantrax@users.noreply.github.com>
2026-04-06 10:32:21 +0000

    addpostcomments and make relations work

--- src/models.py
+++ src/models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+
+#Perdona sergio jajaja el problema de cortar y pegar demasiado xD ya deberia estar areglado
 db = SQLAlchemy()
 
 
